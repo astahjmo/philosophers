bf5e816c866a9a443a374facef3ae035b8307a0b
astahjmo <johmatos@student.42sp.org.br>
2023-08-28 19:28:31 +0200

    chore: update errors msg

--- philo/include/error.h
+++ philo/include/error.h
@@ -6,15 +6,16 @@
 /*   By: johmatos <johmatos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/10 15:35:00 by johmatos          #+#    #+#             */
-/*   Updated: 2023/08/28 17:35:14 by johmatos         ###   ########.fr       */
+/*   Updated: 2023/08/28 19:23:07 by johmatos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ERROR_H
-# define ERROR_H 
-# define WRONG_PARMS "Digita direito alienigena\n" 
-# define NEGATIVE_VAL "Você não pode utilizar um valor negativo!\n"
+# define ERROR_H
+# define WRONG_PARMS "Incorrect parameters. Please use the following format:\n\
+./philo [number_of_philosophers] [time_to_die] [time_to_eat] [time_to_sleep] \
+[times_philo_must_eat] <- optional\n"
+# define NEGATIVE_VAL "You can't use a negative value!\n"
 # define WRONG_QT_PHILO "You need to pass a value between 0 - 200\
  as philo quantity\n"
-# define NEGATIVE_VAL "Você não pode utilizar um valor negativo!\n"
 #endif
